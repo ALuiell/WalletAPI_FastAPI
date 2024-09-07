bbe0bd0e1fea41e413d844deb71d383ade362d25
Aluiel <heaven99902@gmail.com>
2024-09-07 23:07:13 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from uuid import UUID, uuid4
+from datetime import datetime, timezone
+
+
+class Category(BaseModel):
+    id: Optional[int] = None
+    name: str = Field(..., max_length=255)
+
+    class Config:
+        orm_mode = True
+
+
+class UserAccount(BaseModel):
+    id: Optional[int] = None
+    number: str = Field(..., max_length=8)
+    type: str
+    name: str
+    balance: float = 0.0
+
+    class Config:
+        orm_mode = True
+
+
+class Transaction(BaseModel):
+    id: Optional[int] = None
+    number_id: UserAccount = Field(...)
+    type: str
+    category_id: Category = Field(...)
+    date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    transaction_id: UUID = Field(default_factory=uuid4)
+    amount: float
+
+    class Config:
+        orm_mode = True
